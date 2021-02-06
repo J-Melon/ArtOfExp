8965e2e969dbcedfed5d8581c69b874c0cbaa843
Josh Hartley <joh00129@students.stir.ac.uk>
2021-02-06 10:29:29 -0500

    Added README.md and exercises (1-7).

---
+++ Programming/BackToBasics/05-Pointer.c
@@ -0,0 +1,20 @@
+//Uses pointer shenanigans to form different string outputs. 
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str_a[20];
+    char *pointer; //Pointer for string array
+    char *pointer2;
+
+    strcpy(str_a, "Hello world\n");
+    pointer = str_a; //Sets pointer to first array
+    printf(pointer);
+
+    pointer2 = pointer + 2; //Sets the second pointer 2 bytes further in the address memory
+    printf(pointer2);
+    strcpy(pointer2, "y you guys!\n"); //Copy string to new address
+    printf(pointer);
+}
---
+++ Programming/BackToBasics/06-AddressOf.c
@@ -0,0 +1,11 @@
+//Sets the integer pointer to point at the value of the integer variable
+
+#include <stdio.h>
+
+int main(void)
+{
+    int int_var = 5;
+    int *int_ptr;
+
+    int_ptr = &int_var; //Sets address of int_var as the value of int_ptr
+}
