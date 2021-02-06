98814b2090234e0d19bf1390286505d1512a07a6
Josh Hartley <joh00129@students.stir.ac.uk>
2021-02-06 10:29:29 -0500

    Added exercises (1-7).

---
+++ Programming/BackToBasics/03-CharArray2.c
@@ -0,0 +1,13 @@
+//Prints hello world via strcpy'd array.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char str_a[20];
+
+        strcpy(str_a, "Hello world!\n");
+        printf(str_a); //Warning (potentially insecure) only relevant for user-controlled variables. 
+                       //In case of injection.
+}
---
+++ Programming/BackToBasics/04-DatatypeSizes.c
@@ -0,0 +1,15 @@
+//Prints size of memory allocation of various types.
+//Notice how long and long long sizes vary depending on platform.
+
+#include <stdio.h>
+
+int main()
+{
+    printf("The int data type is\t\t %td bytes\n", sizeof(int));
+    printf("The 'unsigned int' data type is\t %td bytes\n", sizeof(unsigned int));
+    printf("The 'short int' data type is\t %td bytes\n", sizeof(short int));
+    printf("The 'long int' data type is\t %td bytes\n", sizeof(long int));   
+    printf("The 'long long int' data type is %td bytes\n", sizeof(long long int));   
+    printf("The 'float' data type is\t %td bytes\n", sizeof(float));   
+    printf("The 'char' data type is\t\t %td bytes\n", sizeof(char));   
+}
