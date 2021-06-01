3062eb97a348bfaaaca7c3fa227a1e42330f88b7
Josh Hartley <joh00129@students.stir.ac.uk>
2021-06-01 10:40:42 -0400

    Finished notes and fixed formatting of PointerTypes2.c.

--- Programming/BackToBasics/12-PointerTypes2.c
+++ Programming/BackToBasics/12-PointerTypes2.c
@@ -1,4 +1,6 @@
-//Program that shows how pointers can be swapped with other data types without causing a crash
+//Program that should iterate across a character array and an integer array but does not compile.
+//Due to incompatible pointer types (when warnings are active).
+//Shows that pointers can be swapped with other data types causing unintended results.
 
 #include <stdio.h>
 
@@ -6,26 +8,26 @@ int main(void)
 {
     int i;
 
-    char char_array[5] = {'a', 'b', 'c', 'd', 'e'};
-    int int_array[5] = {1, 2, 3, 4, 5};
+    char charArray[5] = {'a', 'b', 'c', 'd', 'e'};
+    int intArray[5] = {1, 2, 3, 4, 5};
 
-    char *char_pointer;
-    int *int_pointer;
+    char *charPointer;
+    int *intPointer;
 
-    char_pointer = int_array; // The char_pointer and int_pointer now.
-    int_pointer = char_array; // point to incompatible data types.
+    charPointer = intArray; //The charPointer and intPointer now point to incompatible data types.
+    intPointer = charArray; 
 
     for (i = 0; i < 5; i++)
     {
-        //Iterate through the int array with the int_pointer.
-        printf("[integer pointer] points to %p, which contains the char %c\n", int_pointer, *int_pointer);
-        int_pointer = int_pointer + 1;
+        //Iterate through the int array with the intPointer.
+        printf("[integer pointer] points to %p, which contains the char %c\n", intPointer, *intPointer);
+        intPointer = intPointer + 1;
     }
 
     for (i = 0; i < 5; i++)
     {
-        //Iterate through the char array with the char_pointer.
-        printf("[integer pointer] points to %p, which contains the integer %d\n", char_pointer, *char_pointer);
-        int_pointer = int_pointer + 1;
+        //Iterate through the char array with the charPointer.
+        printf("[integer pointer] points to %p, which contains the integer %d\n", charPointer, *charPointer);
+        intPointer = intPointer + 1;
     }
 }
