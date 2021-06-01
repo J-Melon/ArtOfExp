0e823bcb1c3e8ecb18c5a1591a5a04f350baa310
Josh Hartley <joh00129@students.stir.ac.uk>
2021-06-01 11:05:17 -0400

    Added PointerTypes4.c w/ notes.

---
+++ Programming/BackToBasics/14-PointerTypes4.c
@@ -0,0 +1,36 @@
+//Program that iterates across a character array and an integer array.
+//Shows that a void pointer can be used to iterate across both the character array and the integer array.
+//As typecasting is used.
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    char charArray[5] = {'a', 'b', 'c', 'd','e'};
+    int intArray[5] = {1, 2, 3, 4, 5};
+
+    void *voidPointer;
+
+    voidPointer = (void *) charArray;
+
+    for (i = 0; i < 5; i++)
+    {
+        //Iterate through the char array with the void pointer.
+        printf("[char pointer] points to %p, which contains the char '%c'\n",
+                voidPointer, *((char *) voidPointer));
+        voidPointer = (void *) ((char *) voidPointer + 1);
+    }
+
+    voidPointer = (void *) intArray;
+
+    for (i = 0; i < 5; i++)
+    {
+        //Iterate through the integer array with the void pointer.
+        printf("[integer pointer] points to %p, which contains the integer %d\n",
+                voidPointer, *((int *) voidPointer));
+        voidPointer = (void *) ((int *) voidPointer + 1);
+    }
+}
+
