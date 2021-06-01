cf95caebf9276be3d14e48dcd81cb4a6bef44317
Josh Hartley <joh00129@students.stir.ac.uk>
2021-06-01 08:13:32 -0400

    Changed array size and added notes.

--- Programming/BackToBasics/09-Input.c
+++ Programming/BackToBasics/09-Input.c
@@ -1,9 +1,11 @@
+//Repeats a string n number of times to show the usage of scanf. 
+
 #include <stdio.h>
 #include <string.h>
 
 int main(void)
 {
-    char message[10];
+    char message[13]; //Original array size causes buffer overflow
     int count, i;
 
     strcpy(message, "Hello world!");
