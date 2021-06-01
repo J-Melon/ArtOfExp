055e6c1194a28fe8fa0628afd12f5aa47df0c4fd
Josh Hartley <joh00129@students.stir.ac.uk>
2021-06-01 10:03:14 -0400

    Added PointerTypes1 (original).

---
+++ Programming/BackToBasics/11-PointerTypes1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    char charArray[5] = {'a', 'b', 'c', 'd','e'};
+    int intArray[5] = {1, 2, 3, 4, 5};
+
+    char *char_pointer;
+    int *int_pointer;
+
+    char_pointer = charArray;
+    int_pointer = intArray;
+
+    for (i = 0; i < 5; i++)
+    {
+        //Iterate through the int array with the int_pointer.
+        printf("[integer pointer] points to %p, which contains the integer %d\n", int_pointer, *int_pointer);
+        int_pointer = int_pointer + 1;
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        //Iterate through the char array with the char_pointer.
+        printf("[char pointer] points to %p, which contains the char '%c'\n", char_pointer, *char_pointer);
+        char_pointer = char_pointer + 1;
+    }
+}
+
