b5393435c21c1e90d5849319bd847a7b9aa1946f
Josh Hartley <joh00129@students.stir.ac.uk>
2021-06-02 06:21:39 -0400

    Added Convert2 w/ notes.

---
+++ Programming/BackToBasics/18-Convert2.c
@@ -0,0 +1,30 @@
+//Uses command-line arguments to input and repeat a string n number of times.
+//Shows that a C program without error checking command-line arguments will cause a memory violation.
+//A segmenation fault - When a program tries to access a memory location outside the range of the program.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void useage(char *programName)
+{
+    printf("Usage: %s <message> <# of lines to repeat>\n", programName);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int i, count;
+
+//  if (argc < 3) //If fewer than 3 arguments are used, display usage message and exit.
+//  {
+//      useage(argv[0]);
+//  }
+
+    count = atoi(argv[2]); //Convert the 2nd argument into an integer.
+    printf("Repeating long%d times...\n", count);
+
+    for (i = 0; i < count; i++)
+    {
+        printf("%3d - %s\n", i, argv[1]); //Print the 1st arg.
+    }
+}
