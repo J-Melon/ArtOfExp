9aa92e8456db16d7c533ff41ba6d6bd3c7dfea93
Josh Hartley <joh00129@students.stir.ac.uk>
2021-06-15 05:44:51 -0400

    Added Bitwise w/ notes.

---
+++ Programming/BuildingOnBasics/29-Bitwise.c
@@ -0,0 +1,26 @@
+//Prints the results of OR, AND bitwise operations.
+//Shows that integers can be used to perform logic operations.
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i, bitA, bitB;
+    printf("bitwise OR operator |\n");
+
+    for (i = 0; i < 4; i++)
+    {
+        bitA = (i & 2) / 2; //Get the second bit
+        bitB = (i & 1);
+        printf("%d | %d = %d\n", bitA, bitB, bitA | bitB);
+    }
+
+    printf("\nbitwise AND operator &\n");
+
+    for(i = 0; i < 4; i++)
+    {
+        bitA = (i & 2) / 2; //Get the second bit
+        bitB = (i & 1); //Get the first bitB
+        printf("%d & %d = %d\n", bitA, bitB, bitA & bitB);
+    }
+}
