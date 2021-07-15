520b8f10c925d2c5e1a92627c82dafc1f7207069
Josh Hartley <joh00129@students.stir.ac.uk>
2021-07-15 07:00:54 -0400

    Finished main().

--- Programming/BuildingOnBasics/38-GameOfChance.c
+++ Programming/BuildingOnBasics/38-GameOfChance.c
@@ -46,6 +46,7 @@ int main()
     // Try to read player data from file if no data, register new player
     if(getPlayerData() == -1) { registerNewPlayer(); }
 
+    // Menu select
     while(choice != 7)
     {
         printf("-=[ Game of Chance Menu ]=-\n");
@@ -59,8 +60,43 @@ int main()
         printf("[Name: %s]\n", player.name);
         printf("[You have %u credits] -> ", player.credits);
         scanf("%d", &choice);
+    
+        if ((choice < 1) || (choice > 7)) // Out of range
+        {
+            printf("\n[!!] The number %d is an invalid selection.\n\n", choice);
+        }
+        else if (choice < 4) // Game selection
+        {
+            if (choice != lastGame) // If pointer is not already set to correct function
+            {
+                if (choice == 1) { player.currentGame = pickANumber; } // Point function pointer 
+                else if (choice == 2) { player.currentGame = dealerNoMatch; }
+                else { player.currentGame = findTheAce; }
+                lastGame = choice; // Set last game for ...
+            }
 
+            playTheGame();
+        }
+        else if (choice == 4)
+        {
+            showHighscore();
+        }
+        else if (choice == 5)
+        {
+            printf("\nChange user name\n");
+            printf("Enter your new name: ");
+            inputName();
+            printf("Your name has been changed.\n\n");
+        }
+        else if (choice == 6)
+        {
+            printf("\nYour account has been reset with 100 credits.\n\n");
+            player.credits = 100;
+        }
     }
+
+    updatePlayerData();
+    printf("\nThanks for playing! Bye.\n");
 }
 
 
